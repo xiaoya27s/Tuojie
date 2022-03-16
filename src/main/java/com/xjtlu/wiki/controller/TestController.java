69a2aa52364672023b9c2186e7f6d9b2587f26db
ZengruiLi <473502643@qq.com>
2022-03-17 00:17:30 +0800

    1-2 hello interface

---
+++ src/main/java/com/xjtlu/wiki/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.xjtlu.wiki.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+@RequestMapping("/hello")
+    public String hello(){
+        return "hello world";
+    }
+}
