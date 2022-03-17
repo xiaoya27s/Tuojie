931e017f4d1220cd9adbb2aca1c20c1243c1dfe4
ZengruiLi <473502643@qq.com>
2022-03-17 21:52:50 +0800

    3.1 增加了热部署功能

--- src/main/java/com/xjtlu/wiki/WikiApplication.java
+++ src/main/java/com/xjtlu/wiki/WikiApplication.java
@@ -1,9 +1,11 @@
 package com.xjtlu.wiki;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("package com.xjtlu.wiki.mapper")
 public class WikiApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/xjtlu/wiki/controller/TestController.java
+++ src/main/java/com/xjtlu/wiki/controller/TestController.java
@@ -1,14 +1,29 @@
 package com.xjtlu.wiki.controller;
 
+import com.xjtlu.wiki.entity.Test;
+import com.xjtlu.wiki.service.TestService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.util.List;
+
 @RestController
 public class TestController {
+    @Resource
+    private TestService testService;
 
 @RequestMapping("/hello")
     public String hello(){
         return "hello world";
     }
+
+    @RequestMapping("/test/list")
+    public List<Test> list(){
+
+    return testService.list();
+
+    }
 }
+
