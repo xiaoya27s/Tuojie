c6ca9be3d3fea94b8369f3938051107fd4162b40
ZengruiLi <473502643@qq.com>
2022-03-24 18:31:09 +0800

    4.3 vue cli

--- src/main/java/com/xjtlu/wiki/controller/TestController.java
+++ src/main/java/com/xjtlu/wiki/controller/TestController.java
@@ -3,6 +3,8 @@ package com.xjtlu.wiki.controller;
 import com.xjtlu.wiki.entity.Test;
 import com.xjtlu.wiki.service.TestService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
