96e793342d408322fd7dccd32fa310aa00e71bad
ZengruiLi <473502643@qq.com>
2022-04-14 20:15:07 +0800

    19 - 增加电子书管理页面。 新页面开发

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -1,88 +1,74 @@
 <template>
   <a-layout>
-  <a-layout-sider width="200" style="background: #fff">
-    <a-menu
-        mode="inline"
-        v-model:selectedKeys="selectedKeys2"
-        v-model:openKeys="openKeys"
-        :style="{ height: '100%', borderRight: 0 }"
-    >
-      <a-sub-menu key="sub1">
-        <template #title>
-              <span>
-                <user-outlined />
-                subnav 1
-              </span>
-        </template>
-        <a-menu-item key="1">option1</a-menu-item>
-        <a-menu-item key="2">option2</a-menu-item>
-        <a-menu-item key="3">option3</a-menu-item>
-        <a-menu-item key="4">option4</a-menu-item>
-      </a-sub-menu>
-      <a-sub-menu key="sub2">
-        <template #title>
-              <span>
-                <laptop-outlined />
-                subnav 2
-              </span>
-        </template>
-        <a-menu-item key="5">option5</a-menu-item>
-        <a-menu-item key="6">option6</a-menu-item>
-        <a-menu-item key="7">option7</a-menu-item>
-        <a-menu-item key="8">option8</a-menu-item>
-      </a-sub-menu>
-      <a-sub-menu key="sub3">
-        <template #title>
-              <span>
-                <notification-outlined />
-                subnav 3
-              </span>
-        </template>
-        <a-menu-item key="9">option9</a-menu-item>
-        <a-menu-item key="10">option10</a-menu-item>
-        <a-menu-item key="11">option11</a-menu-item>
-        <a-menu-item key="12">option12</a-menu-item>
-      </a-sub-menu>
-    </a-menu>
-  </a-layout-sider>
-  <a-layout-content
-      :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
-  >
-    <a-list item-layout="vertical" size="large" :grid="{ gutter: 25, column: 3 }" :data-source="ebooks">
-      <template #renderItem="{ item }">
-        <a-list-item key="item.name">
-          <template #actions>
-          <span v-for="{ type, text } in actions" :key="type">
-            <component :is="type" style="margin-right: 8px" />
-            {{ text }}
-          </span>
+    <a-layout-sider width="200" style="background: #fff">
+      <a-menu
+          mode="inline"
+          :style="{ height: '100%', borderRight: 0 }"
+      >
+        <a-sub-menu key="sub1">
+          <template #title>
+            <span><user-outlined />subnav 11111</span>
           </template>
-
-          <a-list-item-meta :description="item.description">
-            <template #title>
-              <a :href="item.href">{{ item.name }}</a>
+          <a-menu-item key="1">option1</a-menu-item>
+          <a-menu-item key="2">option2</a-menu-item>
+          <a-menu-item key="3">option3</a-menu-item>
+          <a-menu-item key="4">option4</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub2">
+          <template #title>
+            <span><laptop-outlined />subnav 2</span>
+          </template>
+          <a-menu-item key="5">option5</a-menu-item>
+          <a-menu-item key="6">option6</a-menu-item>
+          <a-menu-item key="7">option7</a-menu-item>
+          <a-menu-item key="8">option8</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub3">
+          <template #title>
+            <span><notification-outlined />subnav 3</span>
+          </template>
+          <a-menu-item key="9">option9</a-menu-item>
+          <a-menu-item key="10">option10</a-menu-item>
+          <a-menu-item key="11">option11</a-menu-item>
+          <a-menu-item key="12">option12</a-menu-item>
+        </a-sub-menu>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout-content
+        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+    >
+      <a-list item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3 }" :data-source="ebooks">
+        <template #renderItem="{ item }">
+          <a-list-item key="item.name">
+            <template #actions>
+              <span v-for="{ type, text } in actions" :key="type">
+                <component v-bind:is="type" style="margin-right: 8px" />
+                {{ text }}
+              </span>
             </template>
-            <template #avatar><a-avatar :src="item.cover" /></template>
-          </a-list-item-meta>
-        </a-list-item>
-      </template>
-    </a-list>
-  </a-layout-content>
+            <a-list-item-meta :description="item.description">
+              <template #title>
+                <a :href="item.href">{{ item.name }}</a>
+              </template>
+              <template #avatar><a-avatar :src="item.cover"/></template>
+            </a-list-item-meta>
+          </a-list-item>
+        </template>
+      </a-list>
+    </a-layout-content>
   </a-layout>
 </template>
 
 <script lang="ts">
-import { defineComponent,onMounted,ref,reactive } from 'vue';
-import { StarOutlined, LikeOutlined, MessageOutlined } from '@ant-design/icons-vue';
-import axios from "axios";
+import { defineComponent, onMounted, ref, reactive, toRef } from 'vue';
+import axios from 'axios';
 
-const listData: Record<string, string>[] = [];
-
+const listData: any = [];
 for (let i = 0; i < 23; i++) {
   listData.push({
     href: 'https://www.antdv.com/',
     title: `ant design vue part ${i}`,
-    avatar: 'https://joeschmoe.io/api/v1/random',
+    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
     description:
         'Ant Design, a design language for background applications, is refined by Ant UED Team.',
     content:
@@ -92,41 +78,33 @@ for (let i = 0; i < 23; i++) {
 
 export default defineComponent({
   name: 'Home',
-  components: {
-    StarOutlined,
-    LikeOutlined,
-    MessageOutlined,
-  },
-  setup(){
-    console.log("setup");
-    const  ebooks = ref();
+  setup() {
+    const ebooks = ref();
+    const ebooks1 = reactive({books: []});
 
-    const pagination = {
-      onChange: (page: number) => {
-        console.log(page);
-      },
-      pageSize: 3,
-    };
-
-    const actions: Record<string, string>[] = [
-      { type: 'StarOutlined', text: '156' },
-      { type: 'LikeOutlined', text: '156' },
-      { type: 'MessageOutlined', text: '2' },
-    ];
-
-    onMounted( () => {
-      console.log("onMounted")
-      axios.get( "/ebook/list").then(function (response){
+    onMounted(() => {
+      axios.get("/ebook/list").then((response) => {
         const data = response.data;
-        ebooks.value = data.content
-        console.log(response);
+        ebooks.value = data.content;
+        ebooks1.books = data.content;
       });
-    } )
+    });
+
     return {
       ebooks,
+      ebooks2: toRef(ebooks1, "books"),
       listData,
-      pagination,
-      actions,
+      pagination: {
+        onChange: (page: any) => {
+          console.log(page);
+        },
+        pageSize: 3,
+      },
+      actions: [
+        { type: 'StarOutlined', text: '156' },
+        { type: 'LikeOutlined', text: '156' },
+        { type: 'MessageOutlined', text: '2' },
+      ],
     }
   }
 });
